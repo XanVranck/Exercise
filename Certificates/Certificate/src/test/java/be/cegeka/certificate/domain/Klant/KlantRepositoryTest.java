6c619b4c428a41b63ac1989eaed4f1e3f55350b4
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-02-27 21:05:19 +0100

    KlantService getest

---
+++ Certificates/Certificate/src/test/java/be/cegeka/certificate/domain/Klant/KlantRepositoryTest.java
@@ -0,0 +1,21 @@
+package be.cegeka.certificate.domain.Klant;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xanv on 27/02/2017.
+ */
+public class KlantRepositoryTest {
+    @Test
+    public void addKlant() throws Exception {
+
+    }
+
+    @Test
+    public void getKlanten() throws Exception {
+
+    }
+
+}
