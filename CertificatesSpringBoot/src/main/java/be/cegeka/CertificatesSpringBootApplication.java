b4c36d2f05c2e3dea81e594726b6d451c48a73a6
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-02-28 09:05:43 +0100

    Create certificates with springboot

---
+++ CertificatesSpringBoot/src/main/java/be/cegeka/CertificatesSpringBootApplication.java
@@ -0,0 +1,12 @@
+package be.cegeka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificatesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificatesSpringBootApplication.class, args);
+	}
+}
