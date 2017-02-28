bd1d0f0d93d69d1223fb1c73bc83face0fa29a2a
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-02-28 16:13:26 +0100

    AddKlant en getKlant in Klantencontroller

---
+++ Certificate/src/test/java/be/cegeka/controller/KlantController.java
@@ -0,0 +1,33 @@
+package be.cegeka.controller;
+
+import be.cegeka.domain.klant.Klant;
+import be.cegeka.domain.klant.KlantService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Controller
+@RequestMapping("/klant")
+public class KlantController {
+    @Inject
+    private KlantService klantService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public void addKlant(@RequestBody Klant klant){
+        klantService.addKlant(klant);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Klant> getKlanten(){
+        return klantService.getKlanten();
+    }
+
+
+}
