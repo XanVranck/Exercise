97044a3862ed80787da2349bda48eeb8c3cc0509
Xan Vranckaert <xan.vranckaert@cegeka.com>
2017-02-28 16:17:06 +0100

    Added certificaat controller

---
+++ Certificate/src/main/java/be/cegeka/controller/CertificaatController.java
@@ -0,0 +1,34 @@
+package be.cegeka.controller;
+
+import be.cegeka.domain.certificaat.Certificaat;
+import be.cegeka.domain.certificaat.CertificaatService;
+import be.cegeka.domain.klant.Klant;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Controller
+@RequestMapping("/certificaat")
+public class CertificaatController {
+    @Inject
+    private CertificaatService certificaatService;
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    public void addCertificaat(@RequestBody Certificaat certificaat){
+        certificaatService.addCertificate(certificaat);
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public List<Certificaat> getCertificaten(){
+        return certificaatService.getCertificates();
+    }
+
+
+}
